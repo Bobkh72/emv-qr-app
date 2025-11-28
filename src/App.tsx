2161054937df0ac30dee9cdfd7926e8472b1b66d
Bobkh72 <100011071+Bobkh72@users.noreply.github.com>
2025-11-28 17:57:20 +0200

    Fix label formatting for Terminal ID

--- src/App.tsx
+++ src/App.tsx
@@ -182,7 +182,7 @@ function App() {
           />
 
           {/* TCV29 */}
-          <label className="field-label">Terminal ID(29.05)</label>
+          <label className="field-label">Terminal ID (29.05)</label>
           <input className="field-input"
             value={tcv29}
             onChange={(e) => setTcv29(e.target.value)}
